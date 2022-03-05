96981a3fc298d0747df829cb622dbf378cd288c1
RedGry <trottimus.2001@mail.ru>
2022-03-05 23:17:35 +0300

    Simple template

---
+++ src/test/java/com/krivonosovandmarkov/AppTest.java
@@ -0,0 +1,19 @@
+package com.krivonosovandmarkov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    @DisplayName("Test name")
+    void testName() {
+
+        Assertions.fail("Not implemented");
+    }
+}
