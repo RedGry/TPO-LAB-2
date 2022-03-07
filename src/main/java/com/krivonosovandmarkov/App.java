9a05795f1def59b962f396368ca4cce2ce5ae351
RedGry <trottimus.2001@mail.ru>
2022-03-07 16:34:20 +0300

    test

--- src/main/java/com/krivonosovandmarkov/App.java
+++ src/main/java/com/krivonosovandmarkov/App.java
@@ -11,68 +11,68 @@ import java.math.BigDecimal;
 
 public class App {
 
-  public static void main(String[] args) throws IOException {
-    final Cos cos = new Cos();
-    CsvWriter.write(
-        "csv/cos.csv",
-        cos,
-        new BigDecimal(-1),
-        new BigDecimal(1),
-        new BigDecimal("0.1"),
-        new BigDecimal("0.001"));
+    public static void main(String[] args) throws IOException {
+        final Cos cos = new Cos();
+        CsvWriter.write(
+                "csv/cos.csv",
+                cos,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Sin sin = new Sin();
-    CsvWriter.write(
-            "csv/sin.csv",
-            sin,
-            new BigDecimal(-1),
-            new BigDecimal(1),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
+        final Sin sin = new Sin();
+        CsvWriter.write(
+                "csv/sin.csv",
+                sin,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Tan tan = new Tan();
-    CsvWriter.write(
-            "csv/tan.csv",
-            tan,
-            new BigDecimal(-1),
-            new BigDecimal(1),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
+        final Tan tan = new Tan();
+        CsvWriter.write(
+                "csv/tan.csv",
+                tan,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Ln ln = new Ln();
-    CsvWriter.write(
-            "csv/ln.csv",
-            ln,
-            new BigDecimal(1),
-            new BigDecimal(20),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
+        final Ln ln = new Ln();
+        CsvWriter.write(
+                "csv/ln.csv",
+                ln,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Log log3 = new Log(3);
-    CsvWriter.write(
-            "csv/log3.csv",
-            log3,
-            new BigDecimal(1),
-            new BigDecimal(20),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
+        final Log log3 = new Log(3);
+        CsvWriter.write(
+                "csv/log3.csv",
+                log3,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Log log5 = new Log(5);
-    CsvWriter.write(
-            "csv/log5.csv",
-            log5,
-            new BigDecimal(1),
-            new BigDecimal(20),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
+        final Log log5 = new Log(5);
+        CsvWriter.write(
+                "csv/log5.csv",
+                log5,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
 
-    final Log log10 = new Log(10);
-    CsvWriter.write(
-            "csv/log10.csv",
-            log10,
-            new BigDecimal(1),
-            new BigDecimal(20),
-            new BigDecimal("0.1"),
-            new BigDecimal("0.001"));
-  }
+        final Log log10 = new Log(10);
+        CsvWriter.write(
+                "csv/log10.csv",
+                log10,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.001"));
+    }
 }
--- src/main/java/com/krivonosovandmarkov/trigonometric/Cos.java
+++ src/main/java/com/krivonosovandmarkov/trigonometric/Cos.java
@@ -13,36 +13,36 @@ import java.math.MathContext;
 
 public class Cos extends LimitedIterationsExpandableFunction {
 
-  private final Sin sin;
+    private final Sin sin;
 
-  public Cos() {
-    super();
-    this.sin = new Sin();
-  }
-
-  public Cos(final Sin sin) {
-    super();
-    this.sin = sin;
-  }
-
-  @Override
-  public BigDecimal calculate(final BigDecimal x, final BigDecimal precision)
-      throws ArithmeticException {
-    checkValidity(x, precision);
-
-    final MathContext mc = new MathContext(DECIMAL128.getPrecision(), HALF_EVEN);
-    final BigDecimal correctedX = x.remainder(BigDecimalMath.pi(mc).multiply(new BigDecimal(2)));
-
-    if (correctedX.compareTo(ZERO) == 0) {
-      return ONE;
+    public Cos() {
+        super();
+        this.sin = new Sin();
     }
 
-    final BigDecimal result =
-        sin.calculate(
-            BigDecimalMath.pi(mc)
-                .divide(new BigDecimal(2), DECIMAL128.getPrecision(), HALF_EVEN)
-                .subtract(correctedX),
-            precision);
-    return result.setScale(precision.scale(), HALF_EVEN);
-  }
+    public Cos(final Sin sin) {
+        super();
+        this.sin = sin;
+    }
+
+    @Override
+    public BigDecimal calculate(final BigDecimal x, final BigDecimal precision)
+            throws ArithmeticException {
+        checkValidity(x, precision);
+
+        final MathContext mc = new MathContext(DECIMAL128.getPrecision(), HALF_EVEN);
+        final BigDecimal correctedX = x.remainder(BigDecimalMath.pi(mc).multiply(new BigDecimal(2)));
+
+        if (correctedX.compareTo(ZERO) == 0) {
+            return ONE;
+        }
+
+        final BigDecimal result =
+                sin.calculate(
+                        BigDecimalMath.pi(mc)
+                                .divide(new BigDecimal(2), DECIMAL128.getPrecision(), HALF_EVEN)
+                                .subtract(correctedX),
+                        precision);
+        return result.setScale(precision.scale(), HALF_EVEN);
+    }
 }
